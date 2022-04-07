ccbb71f4b8ced288a7cd793448d55cba9055c027
0224488 <0224488@inbox.ru>
2022-04-07 19:10:54 +0300

    Добавьте файлы проекта.

---
+++ LabStructura/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabStructura
+{
+    public struct L11
+    {
+        public double ValB { get; set; }
+        private double valK; // Вводим проверку, поскольку k не должен быть равен 0
+
+        public double ValK
+        {
+            get
+            {
+                return valK;
+            }
+            set
+            {
+                if (value != 0)
+                {
+                    valK = value;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка ввода. Коэффициент k не может быть равен 0");
+                }
+            }
+        }
+
+        public L11(double ValK, double ValB)
+        {
+            valK = ValK;
+            this.ValB = ValB;
+        }
+
+        public void Root()
+        {
+
+            Console.WriteLine("Результат вычислений: x = {0:f5}", (-ValB) / ValK);
+        }
+
+    }
+}
---
+++ PetrovLab11v2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using LabStructura;
+
+namespace PetrovLab11v2
+{
+    class Program
+    {
+        //Разработать структуру для решения линейного уравнения 0=kx+b.
+        //Коэффициенты уравнения k, b реализовать с помощью полей вещественного типа.
+        //Для решения уравнения предусмотреть метод Root. Создать экземпляр разработанной структуры.
+        //Осуществить использование экземпляра в программе.
+        static void Main(string[] args)
+        {
+            Boolean fault = true;
+            L11 structL11 = new L11();
+            try
+            {
+                Console.Write("Введите коэффициент k: ");
+                structL11.ValK = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите коэффициент b: ");
+                structL11.ValB = Convert.ToDouble(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Работа программы завершена из-за ошибки ввода данных. Для выхода нажмите любую клавишу");
+                fault = false; 
+            }
+            if (fault) // Если ошибка сразу при вводе, то не продолжаем
+            {
+                structL11.Root();
+                Console.WriteLine("Для выхода из программы нажмите любую клавишу");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
